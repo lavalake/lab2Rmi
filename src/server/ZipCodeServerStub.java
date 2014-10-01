98071a6bc3426038598500b96a837d5a4f50f626
jason <lavalakewl@gmail.com>
2014-10-01 19:24:31 -0400

    new project

---
+++ src/server/ZipCodeServerStub.java
@@ -0,0 +1,30 @@
+import example.ZipCodeList;
+import example.ZipCodeServer;
+
+public class ZipCodeServerStub implements  ZipCodeServer{
+
+    @Override
+    public void initialise(ZipCodeList newlist) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public String find(String city) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ZipCodeList findAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void printAll() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
---
+++ src/utility/Communication.java
@@ -0,0 +1,5 @@
+package utility;
+public class Communication{
+    String ipAdr;
+    int port;
+}
